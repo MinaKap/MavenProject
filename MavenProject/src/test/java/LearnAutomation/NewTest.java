f63e4ac5464cebfd1a249dc4bbefd2043ceaff0f
meenakshi <meenakshi@WINDOWS-VG46SK4.T-mobile.com>
2019-03-09 12:21:38 -0800

    Commit

---
+++ MavenProject/src/test/java/LearnAutomation/NewTest.java
@@ -0,0 +1,12 @@
+package LearnAutomation;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("hello");
+  }
+  
+}

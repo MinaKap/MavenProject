390e5edcb00d6a167ca8ee984a2d9a2364d5d312
meenakshi <meenakshi@WINDOWS-VG46SK4.T-mobile.com>
2019-03-09 13:11:19 -0800

    New test method added in NewTest

--- MavenProject/src/test/java/LearnAutomation/NewTest.java
+++ MavenProject/src/test/java/LearnAutomation/NewTest.java
@@ -4,9 +4,14 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 
 public class NewTest {
+	//comment
   @Test
   public void f() {
 	  System.out.println("hello");
+  }
+	  @Test
+	  public void g() {
+		  System.out.println("hello");
   }
   
 }
